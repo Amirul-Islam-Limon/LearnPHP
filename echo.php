205f4ddc86b2d06f188dfc663df261ac909eb227
Amirul Islam <bdamirul0@gmail.com>
2023-02-19 19:27:20 +0600

    solved problem

---
+++ echo.php
@@ -0,0 +1,9 @@
+<?php
+    // echo "Hello Everyone";
+    // echo 'Amirul Islam';
+    // echo "Amirul", " Islam";
+    // echo " Amirul Islam"." Limon";
+    echo "<h1><i>Amirul Islam Limon</i></h1>";
+    echo 01515650460;
+
+?>
---
+++ variable.php
@@ -0,0 +1,17 @@
+<?php
+    // $name = "Amirul Islam<br>";
+
+    // echo $name;
+    // echo $name;
+    // echo $name;
+    // echo $name;
+    // echo $name;
+
+    $name = "Amirul Islam";
+    $age = 22;
+
+    echo "You are ". $age . " years old";
+    echo "<h1>" .$name . "</h1>";
+    echo "Hello,  Mr. ". $name;
+
+?>

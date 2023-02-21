efde02c1dab65e91aa1311b8abe33f9b1edc795d
Amirul Islam <bdamirul0@gmail.com>
2023-02-21 22:11:14 +0600

    learn about data type

---
+++ data_types.php
@@ -0,0 +1,14 @@
+<?php
+
+echo " Amirul Islam";
+
+$X = "Alomgir Limon";
+$Y = 33;
+$Z = true;
+$M = array("Amirul", "Islam", "Limon");
+$N = new mySkill();
+$K = null;
+
+$X = 99999;
+var_dump($X);
+?>

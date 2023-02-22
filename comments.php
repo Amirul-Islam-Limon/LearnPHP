6509ef0c37621db719a2e960746e18aaf46db4a4
Amirul Islam <bdamirul0@gmail.com>
2023-02-22 19:49:57 +0600

    learn about comment

---
+++ comments.php
@@ -0,0 +1,13 @@
+<?php
+    echo "This is test!";
+    // echo "This is test!";
+    echo "Amirul Islam";
+    /*
+    echo "Limon Ahmed";
+    echo "I'm From Pabna";
+    */
+    print("I live in Bangladesh");
+
+    $name = "Amir";
+    echo (var_dump($name));
+?>
